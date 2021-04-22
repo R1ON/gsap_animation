8360316508408873d94096c613e10ce9a31dd1cc
Roman Maximov <get.my.email.name@yandex.ru>
2021-04-23 00:07:18 +0300

    day 2

--- src/index.js
+++ src/index.js
@@ -5,56 +5,67 @@ import './index.css';
 
 gsap.registerPlugin(ScrollTrigger);
 
-const t2 = gsap.timeline({
-  scrollTrigger: {
-    trigger: '.container',
-    start: 'top top',
-    end: 'bottom',
-    scrub: 1,
-    pin: true,
-    markers: true,
-  },
-  repeat: 1
-});
-
 const t1 = gsap.timeline({
   scrollTrigger: {
-    trigger: '.container',
+    trigger: '.all-scene',
     start: 'top top',
     end: 'bottom',
-    scrub: 1,
+    scrub: true,
     pin: true,
-    markers: true,
   },
 });
 
-t1.fromTo('.test', {
-  scale: 1.5
-}, {
-  scale: 5
+t1.set('.first-part', {
+  y: '-200%'
+}).set('.left', {
+  x: '-100%'
 })
+.to('.first-part', {
+  y: '0',
+  duration: 1
+}).to('.left', {
+  x: '100%',
+}, '-=1')
 
-t2.fromTo('.test', {
-  y: -30,
-  x: 0,
-}, {
-  x: 50,
-  y: 0,
-  rotation: 10,
-  ease: Power2.easeInOut,
-}).to('.test', {
-  x: 0,
-  y: -30,ease: Power2.easeInOut,
-  rotation: 0,
-}).to('.test', {
-  x: -50,
-  y: 0,ease: Power2.easeInOut,
-  rotation: -10,
-}).to('.test', {
-  x: 0,
-  y: -30,ease: Power2.easeInOut,
-  rotation: 0,
+const t2 = gsap.timeline({
+  repeat: -1,
 });
 
+t2.to('.oblaka', {
+
+  x: 25,
+  ease: Power2.easeInOut
+  })
+  .to('.oblaka', {
+    x: 0,
+    ease: Power2.easeInOut
+  })
+  .to('.oblaka', {
+  x: -25,
+  ease: Power2.easeInOut
+})
+  .to('.oblaka', {
+  x: 0,
+  ease: Power2.easeInOut
+})
+  // .to('.oblaka', {
+  //   x: 90,
+  //   ease: Power2.easeInOut
+  // })
+  // .to('.oblaka', {
+  //   x: -30,
+  //   ease: Power2.easeInOut
+  // })
+
+const scrollHandler = () => {
+  if (window.scrollY > 100) {
+    document.body.className = 'body-background-bottom';
+  }
+  else {
+    document.body.className = 'body-background-top';
+  }
+};
+
+document.addEventListener('scroll', scrollHandler);
 
 
